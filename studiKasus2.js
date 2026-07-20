cfe6107cfe3c451c2b4a91e8da2101ee5e83db7a
zahratihaniaizzah <zahratihaniaizzah@gmail.com>
2026-07-20 15:22:01 +0800

    acharleslove

---
+++ studiKasus2.js
@@ -0,0 +1,20 @@
+//Nilai Ujian
+//Guru ingin mengetahui siapa saja siswa yang lulus.
+const nilai = [90, 75, 60, 45, 88];
+
+let lulus = 0;
+let tidakLulus = 0;
+
+for (let i = 0; i < nilai.length; i++) {
+  if (nilai[i] >= 75) {
+    console.log(`Siswa ke-${i + 1} : ${nilai[i]} -> Lulus`);
+    lulus++;
+  } else {
+    console.log(`Siswa ke-${i + 1} : ${nilai[i]} -> Tidak Lulus`);
+    tidakLulus++;
+  }
+}
+
+console.log("----------------------");
+console.log("Jumlah yang lulus :", lulus);
+console.log("Jumlah yang tidak lulus :", tidakLulus);
