906c7d4fcef0004af8e487673ebe5b240d0c4951
zahratihaniaizzah <zahratihaniaizzah@gmail.com>
2026-07-20 16:25:53 +0800

    sedikitlagi

---
+++ studiKasus5.js
@@ -0,0 +1,24 @@
+//Kehadiran Siswa
+const hadir = [
+true,
+false,
+true,
+true,
+false,
+true
+];
+
+let hadirCount = 0;
+
+for (let i = 0; i <hadir.length; i++) {
+    if (hadir[i] === true) {
+        console.log("Siswa ke-" + (i + 1) + " : Hadir");
+        hadirCount++;
+    } else {
+        console.log("Siswa ke-" + (i + 1) + " : Tidak Hadir");
+    }
+}
+
+console.log("----------------------");
+console.log("Jumlah yang hadir : " + hadirCount);
+console.log("Jumlah yang tidak hadir : " + (hadir.length - hadirCount));
