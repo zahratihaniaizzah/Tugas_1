8bd1b464d30649f26bebd0683b72b14d6e4e52b3
zahratihaniaizzah <zahratihaniaizzah@gmail.com>
2026-07-20 16:46:33 +0800

    hangus

---
+++ studiKasus6.js
@@ -0,0 +1,20 @@
+//Daftar Umur Pengunjung
+const umur = [8, 13, 17, 20, 25, 11];
+let bolehMasuk = 0;
+let  tidakBoleh = 0;
+
+for (let i = 0; i <umur.length; i++) {
+    if (umur[i] >= 17) {
+    console.log("Pengunjung ke-" + (i + 1) + " : Boleh Masuk");
+        bolehMasuk++;
+    } else {
+        console.log("Pengunjung ke-" + (i + 1) + " : Tidak Boleh Masuk");
+        tidakBoleh++;
+    }
+}
+
+
+
+console.log("----------------------");
+console.log("Jumlah yang boleh masuk : " + bolehMasuk);
+console.log("Jumlah yang tidak boleh masuk : " + tidakBoleh);   
