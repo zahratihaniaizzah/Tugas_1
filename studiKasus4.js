a03077dae2581d5eb532eee1ae50f30f8af239f0
zahratihaniaizzah <zahratihaniaizzah@gmail.com>
2026-07-20 16:11:39 +0800

    ohyulpacarku

--- studiKasus4.js
+++ studiKasus4.js
@@ -1,11 +1,20 @@
 //Stok Gudang
 const stok = [12, 0, 5, 8, 0, 20];
 
+let stokHabis = 0;
+let stokTersedia = 0;
+
+
 for (let i = 0; i <stok.length; i++) {
     if (stok[i] === 0) {
         console.log("Barang ke-" + (i + 1) + " : Stok Habis");
+        stokHabis++;
     } else {
         console.log("Barang ke-" + (i + 1) + " : Stok Tersedia");
+        stokTersedia++;
+    }
+ }
 
-    }
-    }
+console.log("----------------------");
+console.log("Jumlah yang stok habis : " + stokHabis);
+console.log("Jumlah yang stok tersedia : " + stokTersedia);
