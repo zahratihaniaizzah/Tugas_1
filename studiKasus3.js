109d2c65db982d431489a7894948f004153f5fea
zahratihaniaizzah <zahratihaniaizzah@gmail.com>
2026-07-20 15:36:22 +0800

    charles P2

---
+++ studiKasus3.js
@@ -0,0 +1,17 @@
+//Daftar Hero Mobile Legends
+const hero = [
+"Layla",
+"Ling",
+"Franco",
+"Fanny",
+"Miya",
+"Eudora"
+]; 
+
+for (let i = 0; i < hero.length; i++) {
+  if (hero[i].startsWith("F")) {
+    console.log(hero[i] + " -> Hero Assassin/Tank Favorit");
+  } else {
+    console.log(hero[i] + " -> Hero Biasa");
+  }
+}
