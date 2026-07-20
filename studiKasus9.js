e3f82f3627c41f99cc81ae07467fa4746f4c8106
zahratihaniaizzah <zahratihaniaizzah@gmail.com>
2026-07-20 19:58:49 +0800

    gatau

---
+++ studiKasus9.js
@@ -0,0 +1,25 @@
+//Data Produk
+
+const produk = [
+  "Laptop",
+  "Mouse",
+  "Keyboard",
+  "Monitor",
+  "Flashdisk"
+];
+
+const harga = [
+  7000000,
+  150000,
+  350000,
+  1800000,
+  120000
+];
+
+for (let i = 0; i < produk.length; i++) {
+  if (harga[i] > 500000) {
+    console.log(produk[i] + " - Rp" + harga[i] + " -> Produk Mahal");
+  } else {
+    console.log(produk[i] + " - Rp" + harga[i] + " -> Produk Murah");
+  }
+}
